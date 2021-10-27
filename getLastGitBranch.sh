de1b0429b1feab48683b4c677bd8e989d6e90a75
HydrolienF <hydrolien.f@gmail.com>
2021-10-27 09:16:22 +0200

    getLastBranch return last branch that look like x.y

--- getLastGitBranch.sh
+++ getLastGitBranch.sh
@@ -1,1 +1,9 @@
-echo $(git branch -r --format="%(refname)" | grep -F '0.' | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- | tail -q -n 1 | tail -c +21)
+i=0
+s="0.1"
+sol=""
+while [[ ! -z $s ]]; do
+  sol=$s
+  s=$(git branch -r --format="%(refname)" | grep -F $i'.' | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- | tail -q -n 1 | tail -c +21)
+  i+=1
+done
+echo $sol
